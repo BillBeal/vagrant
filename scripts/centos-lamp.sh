b55e52790ab4401f5cd7f3e7a0e0921f3e53acb8
Bill Beal <Mr.Mxyztplk@verizon.net>
2014-11-26 20:36:32 -0500

    Updated LAMP script with GitHub files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,6 +30,7 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
-
-
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/BillBeal/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/BillBeal/vagrant/master/files/info.php
 service httpd restart
